3c8c08f8893b9e7fa2eb349cc261971a20d92180
Muhammad Fiqri Alfayed <56479797+MrAisenodni@users.noreply.github.com>
2021-01-17 15:47:07 +0700

    Update navbar.php

--- admin/navbar.php
+++ admin/navbar.php
@@ -116,16 +116,8 @@
                                 <i class="fas fa-clipboard-list" aria-hidden="true"></i><span class="hide-menu">Mapel</span></a>
                         </li>
                         <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                href="materi.php" aria-expanded="false">
-                                <i class="fas fa-clipboard-list" aria-hidden="true"></i><span class="hide-menu">Materi</span></a>
-                        </li>
-                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                href="tugas.php" aria-expanded="false">
-                                <i class="fas fa-clipboard-list" aria-hidden="true"></i><span class="hide-menu">Tugas</span></a>
-                        </li>
-                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
                                 href="admin.php" aria-expanded="false"><i class="fa fa-user"
-                                    aria-hidden="true"></i><span class="hide-menu">Admin</span></a></li>
+                                    aria-hidden="true"></i><span class="hide-menu">Pengguna</span></a></li>
                         <hr>
                         <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
                                 href="login.php" aria-expanded="false"><i class="fas fa-sign-out-alt"
